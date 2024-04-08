5760a88bec43ed40133a7131504f50f0c2091c09
Mathis Delsart <mathis.delsart@student.uclouvain.be>
2024-04-08 16:24:46 +0200

    Update docstrings and refactor code

--- Assignement 3/namazon.py
+++ Assignement 3/namazon.py
@@ -1,161 +1,212 @@
 from search import *
 import time
 
-#################
-# Problem class #
-#################
-
 """
-The problem of placing N amazons on an NxN board with none attacking
-each other. A state is represented as an N-element array, where
-a value of r in the c-th entry means there is an empress at column c,
-row r, and a value of -1 means that the c-th column has not been
-filled in yet. We fill in columns left to right.
+NAmazonsProblem class for the NAmazons problem.
+
+The problem is to place N amazons on an NxN board with none attacking each other.
+The board must be filled column by column, from left to right.
+The rules are the following:
+    - No two amazons can be in the same row
+    - No two amazons can be in the same diagonal
+    - No two amazons can be in the same 'special knight moves' (2x3/3x2/4x1/1x4)
 """
 class NAmazonsProblem(Problem):
     
+    """
+    Attributes:
+        - initial_state: the initial state of the problem
+        - goal_state: NOT USED (because the goal to this problem is not a specific state)
+        - N: the size of the board
+    """
     def __init__(self, N, initial_state):
         Problem.__init__(self, initial_state)
         self.N = N
 
+    """
+    Get the possible actions from a state.
+
+    An action is a row index where an empress can be placed in the current column.
+    The current column is the first empty column in the state and is specified in its attributes.
+    The action must respect the rules of the problem.
+
+    @param state: The current state of the problem.
+    @return: The list of possible actions.
+    """
     def actions(self, state):
-        actions = [i for i in range(self.N) if i not in state.rows]
-        filtered_actions = []
-        for action in actions:
+        actions = []
+        for action in range(self.N):
+            # Check if the action is valid
             valid_action = True
-            for i in range(state.colEmpty):
-                currRow = state.rows[i]
-                # Check the same row
-                if currRow == action: valid_action = False; break
-                diff_col = state.colEmpty - i
-                diff_row = abs(currRow - action)
-                # Check the same diagonal
-                if diff_row == diff_col: valid_action = False; break
-                # Check the special 'knight moves'
-                if diff_col == 1 and diff_row == 4: valid_action = False; break
-                if diff_col == 4 and diff_row == 1: valid_action = False; break
-                if diff_col == 2 and diff_row == 3: valid_action = False; break
-                if diff_col == 3 and diff_row == 2: valid_action = False; break
-
-            if valid_action:
-                filtered_actions.append(action)
-
-        return filtered_actions
+            for col in range(state.colEmpty):
+                diff_col = state.colEmpty - col
+                diff_row = abs(state.rows[col] - action)
+                if diff_row == 0 or diff_row == diff_col or diff_row == diff_col: valid_action = False; break # Same row / column or diagonal
+                if diff_col == 1 and diff_row == 4: valid_action = False; break # Special 'knight moves' 1x4
+                if diff_col == 4 and diff_row == 1: valid_action = False; break # Special 'knight moves' 4x1
+                if diff_col == 2 and diff_row == 3: valid_action = False; break # Special 'knight moves' 2x3
+                if diff_col == 3 and diff_row == 2: valid_action = False; break # Special 'knight moves' 3x2
+            
+            # Add the action if it is valid
+            if valid_action: actions.append(action)
+        return actions
     
+    """
+    Apply an action to a state and return the new state.
+    The action to apply is to place an empress in the current column at the specified row.
+
+    @param state: The current state of the problem.
+    @param row: The row index where to place the empress.
+
+    @return: The new state after applying the action.
+    """
     def result(self, state, row):
         rows = state.rows.copy()
         rows[state.colEmpty] = row
         state = State(rows)
         return state
 
+    """
+    Check if a state is a goal state.
+    A state is a goal state if the last column are filled with an empress (and thus all the columns through the modeling of the problem).
+
+    @param state: The state to check.
+
+    @return: True if the state is a goal state, False otherwise.
+    """
     def goal_test(self, state):
         return True if state.colEmpty == state.N else False
 
-    def h(self, node):
-        nbAttacks = 0
-        rows = node.state.rows
-        for i in range(node.state.colEmpty):
-            currRow = rows[i]
-            for j in range(node.state.colEmpty):
-                if i == j: continue # No need to check the same column (by construction of the problem)
-                otherRow = rows[j]
-                if currRow == otherRow: nbAttacks += 1 # Same row
-                if abs(currRow - otherRow) == abs(i - j): nbAttacks += 1 # Same diagonal
-                # Special 'knight moves'
-                if abs(i - j) == 1 and abs(currRow - otherRow) == 4: nbAttacks += 1
-                if abs(i - j) == 4 and abs(currRow - otherRow) == 1: nbAttacks += 1
-                if abs(i - j) == 3 and abs(currRow - otherRow) == 2: nbAttacks += 1
-                if abs(i - j) == 2 and abs(currRow - otherRow) == 3: nbAttacks += 1
-        return nbAttacks
+    """
+    Heuristic function for the NAmazonsProblem.
+    The heuristic is the number of attacks between the empresses on the board.
     
-###############
-# State class #
-###############
+    @param node: The node to evaluate.
+    @return: The heuristic value of the node.
+    """
+    def h(self, node):
+        conflicts = 0
+        rows = node.state.rows
+        # Iterate over the filled column
+        for i in range(node.state.colEmpty):
+            # Iterate to the right of the i_th column (stop to the first empty column)
+            for j in range(i + 1, node.state.colEmpty):
+                diff_col = j - i
+                diff_row = abs(rows[i] - rows[j])
+                if diff_row == 0:        conflicts += 1 # Same row
+                if diff_row == diff_col: conflicts += 1 # Same diagonal
+                if diff_col == 1 and diff_row == 4: conflicts += 1 # Special 'knight moves' 1x4
+                if diff_col == 4 and diff_row == 1: conflicts += 1 # Special 'knight moves' 4x1
+                if diff_col == 3 and diff_row == 2: conflicts += 1 # Special 'knight moves' 3x2
+                if diff_col == 2 and diff_row == 3: conflicts += 1 # Special 'knight moves' 2x3
+        return conflicts
+    
 
+"""
+State representation for the NAmazonsProblem.
+
+The state is represented as an N-element array, where a value of r in the c-th entry means there is an empress at column c, row r,
+and a value of -1 means that the c-th column has not been filled in yet. We fill in columns left to right.
+"""
 class State:
 
+    """
+    Attributes:
+        - rows: an N-element array
+        - N: the size of the board
+        - colEmpty: the index of the first empty column
+    """
     def __init__(self, rows):
         self.rows = rows
         self.N = len(rows)
         self.colEmpty = self.N if (self.rows[self.N - 1] != -1) else self.rows.index(-1)
     
+    """Equality methods for comparison and hashing"""
     def __eq__(self, other):
         return isinstance(other, State) and (self.rows == other.rows)
 
+    """Less than method for priority queue"""
     def __lt__(self, other):
         return self.colEmpty < other.colEmpty
     
+    """Hash method for hashing in the priority queue"""
     def __hash__(self):
         return hash(tuple(self.rows))
     
+    """String representation of the state for formatting the output"""
     def __str__(self):
         str_repr = ""
-        listRows = ['#' * self.N] * self.N
+        rows_str = ['#' * self.N] * self.N
         for i in range(self.N):
             row = self.rows[i]
-            if row == -1:
-                break
-            listRows[row] = '#' * i + 'A' + '#' * (self.N - i - 1)
-        for row in listRows:
-            str_repr += row + '\n'
+            if row == -1: break # Already created the empty columns in the 'rows_str'
+            rows_str[row] = '#' * i + 'A' + '#' * (self.N - i - 1) # Replace the '#' by 'A' in the row
+        # Create the complete string representation
+        for row in rows_str: str_repr += row + '\n'
         return str_repr[:-1]
-        
 
-#####################
-# Launch the search #
-#####################
 
-def analyseTime(search_function):
+"""
+Analyse the time and the number of moves to solve the NAmazonsProblem for a value of N with a given search function.
+The results are printed in the console.
+
+@param N: The size of the board (must be > 0).
+@param search_function: The search function to use to solve the problem. Default is depth_first_graph_search.
+"""
+def analyseSolver(N, search_function=depth_first_graph_search):
+    # Create the NAzamonsProblem
+    initial_state = State([-1 for _ in range(N)])
+    problem = NAmazonsProblem(N, initial_state)
+
+    # Solve the problem
+    start_timer = time.perf_counter()
+    node = search_function(problem)
+    end_timer = time.perf_counter()
+
+    # Print the time and the number of moves
+    print("Time: ", end_timer - start_timer)
+    print('Number of moves: ', str(node.depth))
+
+    # For debugging purposes
+    # path = node.path()
+    # for n in path:
+    #     print(n.state)
+    #     print()
+
+"""
+Analyse the time and the number of moves to solve the NAmazonsProblem for different values of N.
+This analysis is done for the depth_first_graph_search, astar_search and breadth_first_graph_search functions.
+
+The results are printed in the console.
+"""
+def perfs_analyser():
     N = [10, 11, 12, 13, 20, 25, 30]
-    for n in N:
-        initial_state = State([-1 for _ in range(n)])
-        problem = NAmazonsProblem(n, initial_state)
-        start_timer = time.perf_counter()
-        node = search_function(problem)
-        end_timer = time.perf_counter()
-        print("Time: ", end_timer - start_timer)
-        path = node.path()
-        print('Number of moves: ', str(node.depth))
-        # for n in path:
-        #     print(n.state)
-        #     print()
+    for n in N: analyseSolver(n, depth_first_graph_search)
+    # for n in N: analyseSolver(n, astar_search)
+    # for n in N: analyseSolver(n, breadth_first_graph_search)
 
-# analyseTime(breadth_first_graph_search)
-# analyseTime(depth_first_graph_search)
-# analyseTime(astar_search)
 
-N = int(sys.argv[1])
-initial_state = State([-1 for _ in range(N)])
-problem = NAmazonsProblem(N, initial_state)
+# Main function
+if __name__ == "__main__":
 
-start_timer = time.perf_counter()
+    # perfs_analyser()
 
-node = depth_first_graph_search(problem)
+    N = int(sys.argv[1]) # Get the size of the board from the command line
 
-end_timer = time.perf_counter()
+    # Create the NAmazonsProblem
+    initial_state = State([-1 for _ in range(N)])
+    problem = NAmazonsProblem(N, initial_state)
 
-# path = node.path()
-# print('Number of moves: ', str(node.depth))
-# for n in path:
-#     print(n.state) # assuming that the _str_ function of state outputs the correct format
-#     print()
-# print("Time: ", end_timer - start_timer)
+    # Solve the problem
+    start_timer = time.perf_counter()
+    node = astar_search(problem)
+    end_timer = time.perf_counter()
 
-# path = node.path()
-# print("initial state")
-# print(path[0].state)
-# print()
-# for i in range(1, len(path) - 1):
-#     n = path[i]
-#     print("state {}".format(i))
-#     print(n.state)
-#     print()
-# n = path[len(path) - 1]
-# print("goal state")
-# print(n.state)
+    # Print the solution
+    path = node.path()
+    print('Number of moves: ', str(node.depth))
+    for n in path:
+        print(n.state)
+        print()
 
-path = node.path()
-print('Number of moves: ', str(node.depth))
-for n in path:
-    print(n.state)  # assuming that the _str_ function of state outputs the correct format
-    print()
+    print("Time: ", end_timer - start_timer) # To remove for the submission
--- Assignement 3/sudoku.py
+++ Assignement 3/sudoku.py
@@ -2,78 +2,98 @@ import random
 import time
 import math
 import sys
+import collections
 
+"""
+Get the objective score of the Sudoku board.
+
+The objective score is the sum of the number of conflicts in each row, column, and subgrid,
+plus the number of empty tiles in the board.
+
+A conflict is the number of times a number appears more than once in a row, column, or subgrid.
+
+@param board: The Sudoku board
+@return: The objective score of the board
+"""
 def objective_score(board):
 
-    def getTilesEmpty(board):
-        empty_tiles = 0
-        for row in board: empty_tiles += row.count(0)
-        return empty_tiles
+    """Get the number of empty tiles on the board."""
+    def getTilesEmpty():
+        return sum(row.count(0) for row in board)
 
-    def getConflictsRow(board, row):
-        conflicts = 0
-        rowElements = board[row]
-        for i in range(1, 10):
-            nbElement = rowElements.count(i)
-            if nbElement > 1: conflicts += nbElement
-        return conflicts
+    """Get the number of conflicts in a row of the board."""
+    def getConflictsRow(row):
+        return sum(count - 1 for count in collections.Counter(board[row]).values() if count > 1)
 
-    def getConflictsColumn(board, col):
-        conflicts = 0
-        columnElements = [board[i][col] for i in range(9)]
-        for i in range(1, 10):
-            nbElement = columnElements.count(i)
-            if nbElement > 1: conflicts += nbElement
-        return conflicts
+    """Get the number of conflicts in a column of the board."""
+    def getConflictsColumn(col):
+        return sum(count - 1 for count in collections.Counter([board[i][col] for i in range(9)]).values() if count > 1)
 
-    def getConflictsSubgrid(board, idx):
-        subgridElements = []
-        pos_i, pos_j = 3 * (idx // 3), 3 * (idx % 3)
-        for k in range(3):
-            for l in range(3):
-                subgridElements.append(board[pos_i + k][pos_j + l])
-        conflicts = 0
-        for i in range(1, 10):
-            nbElement = subgridElements.count(i)
-            if nbElement > 1: conflicts += nbElement
-        return conflicts
+    """Get the number of conflicts in a subgrid of the board."""
+    def getConflictsSubgrid(idx):
+        subgridElements = [board[3 * (idx // 3) + k][3 * (idx % 3) + l] for k in range(3) for l in range(3)]
+        return sum(count - 1 for count in collections.Counter(subgridElements).values() if count > 1)
 
-    conflicts = 0
-    empty_tiles = getTilesEmpty(board)
-    for i in range(9):
-        conflicts += getConflictsRow(board, i)
-        conflicts += getConflictsColumn(board, i)
-        conflicts += getConflictsSubgrid(board, i)
+    empty_tiles = getTilesEmpty()
+    conflicts = sum(getConflictsRow(i) + getConflictsColumn(i) + getConflictsSubgrid(i) for i in range(9))
     return conflicts + empty_tiles
 
+
+"""
+Get the available values that can be placed in a tile of the Sudoku board without create conflicts.
+
+@param board: The Sudoku board
+@param i: The row index of the tile
+@param j: The column index of the tile
+
+@return: The list of available values that can be placed in the tile
+"""
 def getAvailableValues(board, i, j):
     row = board[i]
     col = [board[k][j] for k in range(9)]
-    subgrid = []
-    pos_i, pos_j = 3 * (i // 3), 3 * (j // 3)
-    for k in range(3):
-        for l in range(3):
-            subgrid.append(board[pos_i + k][pos_j + l])
+    subgrid = [board[3 * (i // 3) + k][3 * (j // 3) + l] for k in range(3) for l in range(3)]
+    # Substract the values already present in the row, column, and subgrid from the set of all possible values
     available_values = set(range(1, 10)) - set(row + col + subgrid)
-    return list(available_values)
+    return available_values
 
-def generate_neighbor(current_solution, fixed_positions):
-    listValues = []
+"""
+Generate a neighboring solution by randomly selecting a tile and changing its value.
+The new value is chosen randomly from the set of values that can be legally placed in that tile based on Sudoku rules.
+If no such value is available within a specified maximum number of iterations (MAX_ITER),
+a random value between 1 and 9 is selected, potentially introducing conflicts.
+
+@param current_solution: The current Sudoku board
+@param fixed_positions: The set of fixed positions in the board
+
+@return: The neighbor of the current solution
+"""
+def generate_neighbor(current_solution, fixed_positions, MAX_ITER=5):
+    # Randomly select a tile and get the available values that can be placed in that tile
+    values = set()
     i, j = random.randint(0, 8), random.randint(0, 8)
-    MAX_ITER = 5
-    incr = 0
-    while len(listValues) == 0:
-        incr += 1
-        while (i, j) in fixed_positions:
+    nbIteration = 0
+    while len(values) == 0 and nbIteration < MAX_ITER: # Avoid getting an empty list of available values
+        nbIteration += 1
+        while (i, j) in fixed_positions: # Avoid changing the value of a fixed tile
             i, j = random.randint(0, 8), random.randint(0, 8)
-        listValues = getAvailableValues(current_solution, i, j)
-        if incr == MAX_ITER: listValues = list(range(1, 10))
+        values = getAvailableValues(current_solution, i, j)
+    if nbIteration == MAX_ITER: values = set(range(1, 10)) # If no available values, select a random value (after MAX_ITER iterations)
 
-    new_value = random.choice(listValues)
+    # Randomly select a new value for the tile from the available values
+    new_value = random.choice(list(values))
+
+    # Copy the current solution and update the value of the selected tile to create the neighbor
     neighbor = [row[:] for row in current_solution]
     neighbor[i][j] = new_value
     return neighbor
 
+"""
+Get the set of fixed positions in the Sudoku board.
+
+@param initial_board: The initial Sudoku board
+
+@return: The set of fixed positions in the board
+"""
 def get_fixed_positions(initial_board):
     fixed_positions = set()
     for i in range(9):
@@ -85,6 +105,7 @@ def get_fixed_positions(initial_board):
 Simulated annealing Sudoku solver.
 """
 def simulated_annealing_solver(initial_board):
+    # Get the set of fixed positions in the board
     fixed_positions = get_fixed_positions(initial_board)
 
     current_solution = [row[:] for row in initial_board]
@@ -95,10 +116,11 @@ def simulated_annealing_solver(initial_board):
 
     temperature = 1.0
     threshold_T = 0.0001
-    cooling_rate = 0.9999999999 # To be adjusted
+    cooling_rate = 0.9999999999 # To have a very slow cooling rate (maximum iterations)
 
     while temperature > threshold_T:
         try:
+            # Generate a neighbor solution
             neighbor = generate_neighbor(current_solution, fixed_positions)
 
             # Evaluate the neighbor
@@ -107,6 +129,7 @@ def simulated_annealing_solver(initial_board):
             # Calculate acceptance probability
             delta = float(current_score - neighbor_score)
 
+            # If the current score is 0, return the current solution and his score
             if current_score == 0: return current_solution, current_score
             
             # Accept the neighbor with a probability based on the acceptance probability
@@ -127,25 +150,36 @@ def simulated_annealing_solver(initial_board):
 
 """
 Print the Sudoku board.
+
+@param board: The Sudoku board
+@return: None
 """
 def print_board(board):
     for row in board: print("".join(map(str, row)))
  
 """
 Read Sudoku puzzle from a text file.
+
+@param file_path: The path to the text file containing the Sudoku puzzle
+@return: The Sudoku board
 """
 def read_sudoku_from_file(file_path):
     with open(file_path, 'r') as file:
         sudoku = [[int(num) for num in line.strip()] for line in file]
     return sudoku
 
-
+# Main function
 if __name__ == "__main__":
 
+    # Read the Sudoku puzzle from the text file
     initial_board = read_sudoku_from_file(sys.argv[1])
+
+    # Solve the Sudoku puzzle using simulated annealing
     start_timer = time.perf_counter()
     solved_board, current_score = simulated_annealing_solver(initial_board)
     end_timer = time.perf_counter()
+
+    # Print the solved Sudoku board and the objective score
     print_board(solved_board)
     print("\nValue(C):", current_score)
 
